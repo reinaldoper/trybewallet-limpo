6d5d90634f3549dc10615130e96a4d6a051d8732
Reinaldo <reinaldoper83@gmail.com>
2022-08-29 20:33:04 -0300

    ajustando valores retornados da API

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -10,31 +10,22 @@ class Table extends Component {
       const { value, currency, exchangeRates, description, tag, method } = item;
       const { name, ask } = exchangeRates[currency];
       const result = ask * value;
-      const convertReal = Math.round((result * ask) * 100) / 100;
-      const soma = Math.round(result * 100) / 100;
+      const valor = `${value}.00`;
+      const convertMoeda = Math.round(ask * 100) / 100;
+      const convertReal = Math.round((result) * 100) / 100;
+      /* const soma = Math.round(result * 100) / 100; */
       return (
         <tbody key={ index }>
-          <tr>
-            <td>Descrição</td>
-            <td>Tag</td>
-            <td>Método de pagamento</td>
-            <td>Valor</td>
-            <td>Moeda</td>
-            <td>Câmbio utilizado</td>
-            <td>Valor convertido</td>
-            <td>Moeda de conversão</td>
-            <td>Editar/Excluir</td>
-          </tr>
           <tr>
             <td>{description}</td>
             <td>{tag}</td>
             <td>{method}</td>
-            <td>{soma}</td>
+            <td>{valor}</td>
             <td>{name}</td>
-            <td>{currency}</td>
+            <td>{convertMoeda}</td>
             <td>{convertReal}</td>
             <td>Real</td>
-            <td>{' '}</td>
+            {/* <td>{' '}</td> */}
           </tr>
         </tbody>
       );
