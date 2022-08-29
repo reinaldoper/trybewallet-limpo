74350ebece24759f2a2137096b6435d70f6efe78
Reinaldo <reinaldoper83@gmail.com>
2022-08-29 17:22:41 -0300

    ajustando o <th> da tabela

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -6,15 +6,15 @@ class Table extends Component {
       <div>
         <table border="1">
           <tr>
-            <td>Descrição</td>
-            <td>Tag</td>
-            <td>Método de pagamento</td>
-            <td>Valor</td>
-            <td>Moeda</td>
-            <td>Câmbio utilizado</td>
-            <td>Valor convertido</td>
-            <td>Moeda de conversão</td>
-            <td>Editar/Excluir</td>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
           </tr>
         </table>
       </div>
